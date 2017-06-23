c2188475b479615e546af7d27d183f8232b9f273
Jeff Bell <jeferb9@gmail.com>
2017-06-22 20:53:33 -0700

    Cors fix

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.listen(port, function() {
