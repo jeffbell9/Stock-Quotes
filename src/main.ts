8410daeab7a0ab63150e5d4364696af6c3c9e8c8
Jeff Bell <jeferb9@gmail.com>
2017-06-22 17:11:12 -0700

    Enabled production mode

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './styles/main.css';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
